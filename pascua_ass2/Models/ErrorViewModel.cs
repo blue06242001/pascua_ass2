67959b3d01b4019c18d030e12ce417fda1e66bc1
QCU <QCU@DESKTOP-R04522K>
2024-02-26 12:06:22 +0800

    Add project files.

---
+++ pascua_ass2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pascua_ass2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
